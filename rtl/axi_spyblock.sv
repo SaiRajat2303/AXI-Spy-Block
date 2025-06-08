d6195e1708be3aaa84e0a1fb75d2b38d4efdee6e
Sai Rajat <87222373+SaiRajat2303@users.noreply.github.com>
2025-06-09 02:20:46 +0530

    Bug Fix : Handle cases with back to back axi transaction traffic

--- rtl/axi_spyblock.sv
+++ rtl/axi_spyblock.sv
@@ -8,21 +8,25 @@ module axi_spyblock #(
     // AXI AR channel signals 
     input ARVALID,
     input ARREADY,
+    input [3:0] ARID,
     input [ADDR_WIDTH-1:0] ARADDR,
 
     // AXI AW channel signals
     input AWVALID,
     input AWREADY,
+    input [3:0] AWID,
     input [ADDR_WIDTH-1:0] AWADDR,
 
     // AXI W channel signals
     input WVALID,
     input WREADY,
+    input [3:0] WID,
     input [DATA_WIDTH-1:0] WDATA,
 
     // AXI R channel signals 
     input RVALID,
     input RREADY,
+    input [3:0] RID,
     input [DATA_WIDTH-1:0] RDATA,
 
     // Generic signals
@@ -36,20 +40,29 @@ module axi_spyblock #(
     output aw_spy_full
 );
 
+// Detect a valid txn using valid - Ready Handshake mechanism
+wire ar_handshake;
+wire aw_handshake;
+wire w_handshake;
+wire r_handshake;
+
+assign ar_handshake     = (ARREADY & ARVALID);
+assign aw_handshake     = (AWREADY & AWVALID);
+assign w_handshake      = (WREADY & WVALID);
+assign r_handshake      = (RREADY & RVALID);
+
+// We dont really need a reset handling here for spy fifos , as they reset on their own
+
+// ------------------------------------------------ R SPY FIFO HANDLING LOGIC ------------------------------------------
+
 // FIFO signals for R-channel spy block
-reg [DATA_WIDTH-1:0] rdat;
+reg [DATA_WIDTH-1:0] r_data;
 reg r_push;
 reg r_pop; // assert -> when fifo is full and the next negedge is triggered
 wire [DATA_WIDTH-1:0] r_pop_data;
 wire r_full;
 wire r_empty;
-wire r_trig;
-reg r_trig_d1;
-wire r_trig_neg;
-
-assign r_trig = (RREADY == 1'b1)?(RREADY ^ RVALID):(1'b1);
-assign r_trig_neg = r_trig_d1 & ~r_trig; // negative edge detector
-assign r_spy_full = r_full;
+reg [3:0] prev_rid;
 
 fifo #(
     .DATA_WIDTH(DATA_WIDTH),
@@ -59,12 +72,38 @@ fifo #(
     .clk(clk),
     .reset(reset),
     .push_i(r_push),
-    .push_data_i(rdat),
+    .push_data_i(r_data),
     .pop_i(r_pop),
     .pop_data_o(r_pop_data),
     .full_o(r_full),
     .empty_o(r_empty)
 );
+
+always@(posedge clk) begin
+    if(r_handshake) begin
+        if(prev_rid !== RID) begin
+            prev_rid <= RID; // Store the current ID for next comparison
+            r_push <= 1'b1; // Set push flag for new ID
+            r_data <= RDATA;
+            if(r_full) begin
+                r_pop <= 1'b1; // Pop data if FIFO is full
+            end
+            else begin
+                r_pop <= 1'b0;
+            end
+        end 
+        else begin
+            prev_rid <= prev_rid; // Maintain previous arid
+            r_push <= 1'b0; // Reset push flag if no new IDß
+        end
+    end
+    else begin
+        r_push <= 1'b0; // Reset push flag if no handshake
+    end
+    
+end
+
+// ------------------------------------------------ AR SPY FIFO HANDLING LOGIC ------------------------------------------
 
 // FIFO signals for AR-channel spy block
 reg [DATA_WIDTH-1:0] ar_addr;
@@ -73,12 +112,8 @@ reg ar_pop; // assert -> when fifo is full and the next negedge is triggered
 wire [DATA_WIDTH-1:0] ar_pop_addr;
 wire ar_full;
 wire ar_empty;
-wire ar_trig;
-reg ar_trig_d1;
-wire ar_trig_neg;
+reg [3:0] prev_arid; // to store the previous ARID for comparison
 
-assign ar_trig = (ARREADY == 1'b1)?(ARREADY ^ ARVALID):(1'b1);
-assign ar_trig_neg = ar_trig_d1 & ~ar_trig; // negative edge detector
 assign ar_spy_full = ar_full;
 
 fifo #(
@@ -96,6 +131,31 @@ fifo #(
     .empty_o(ar_empty)
 );
 
+always@(posedge clk) begin
+    if(ar_handshake) begin
+        if(prev_arid !== ARID) begin
+            prev_arid <= ARID; // Store the current ID for next comparison
+            ar_push <= 1'b1; // Set push flag for new ID
+            ar_addr <= ARADDR;
+            if(ar_full) begin
+                ar_pop <= 1'b1; // Pop data if FIFO is full
+            end 
+            else begin
+                ar_pop <= 1'b0;
+            end
+        end 
+            else begin
+                prev_arid <= prev_arid; // Maintain previous arid
+                ar_push <= 1'b0; // Reset push flag if no new IDß
+            end
+    end
+    else begin
+        ar_push <= 1'b0; // Reset push flag if no handshake
+    end
+end
+
+// ------------------------------------------------ AW SPY FIFO HANDLING LOGIC ------------------------------------------
+
 // FIFO Signals for AW-channel spy block
 reg [DATA_WIDTH-1:0] aw_addr;
 reg aw_push;
@@ -103,12 +163,8 @@ reg aw_pop; // assert -> when fifo is full and the next negedge is triggered
 wire [DATA_WIDTH-1:0] aw_pop_addr;
 wire aw_full;
 wire aw_empty;
-wire aw_trig;
-reg aw_trig_d1;
-wire aw_trig_neg;
+reg [3:0] prev_awid; // to store the previous AWID for comparison
 
-assign aw_trig = (ARREADY == 1'b1)?(ARREADY ^ ARVALID):(1'b1);
-assign aw_trig_neg = aw_trig_d1 & ~aw_trig; // negative edge detector
 assign aw_spy_full = aw_full;
 
 fifo #(
@@ -126,6 +182,31 @@ fifo #(
     .empty_o(aw_empty)
 );
 
+always@(posedge clk) begin
+    if(aw_handshake) begin
+        if(prev_awid !== AWID) begin
+            prev_awid <= AWID; // Store the current ID for next comparison
+            aw_push <= 1'b1; // Set push flag for new ID
+            aw_addr <= AWADDR;
+            if(aw_full) begin
+                aw_pop <= 1'b1; // Pop data if FIFO is full
+            end 
+            else begin
+                aw_pop <= 1'b0;
+            end
+        end 
+        else begin
+            prev_awid <= prev_awid; // Maintain previous awid
+            aw_push <= 1'b0; // Reset push flag if no new IDß
+        end
+    end
+    else begin
+        aw_push <= 1'b0; // Reset push flag if no handshake
+    end
+end
+
+// ------------------------------------------------ W SPY FIFO HANDLING LOGIC ------------------------------------------
+
 // FIFO Signals for W-channel spy block
 reg [DATA_WIDTH-1:0] w_data;
 reg w_push;
@@ -133,13 +214,7 @@ reg w_pop; // assert -> when fifo is full and the next negedge is triggered
 wire [DATA_WIDTH-1:0] w_pop_data;
 wire w_full;
 wire w_empty;
-wire w_trig;
-reg w_trig_d1;
-wire w_trig_neg;
-
-assign w_trig = (WREADY == 1'b1)?(WREADY ^ WVALID):(1'b1);
-assign w_trig_neg = w_trig_d1 & ~w_trig; // negative edge detector
-assign w_spy_full = w_full;
+reg [3:0] prev_wid;
 
 fifo #(
     .DATA_WIDTH(ADDR_WIDTH),
@@ -156,80 +231,29 @@ fifo #(
     .empty_o(w_empty)
 );
 
-// We dont really need a reset handling here for spy fifos , as they reset on their own
-
 always@(posedge clk) begin
-    // R spyblock handling
-    r_trig_d1 <= r_trig; // 1 cycle delayed version of r_trig
-    ar_trig_d1 <= ar_trig; 
-    aw_trig_d1 <= aw_trig;
-    w_trig_d1 <= w_trig;
-
-    if(r_trig_neg) begin
-        rdat <= RDATA;
-        r_push <= 1'b1;
-        if(r_full) begin
-            r_pop <= 1'b1;
+    if(w_handshake) begin
+        if(prev_wid !== WID) begin
+            prev_wid <= WID; // Store the current ID for next comparison
+            w_push <= 1'b1; // Set push flag for new ID
+            w_data <= WDATA;
+            if(w_full) begin
+                w_pop <= 1'b1; // Pop data if FIFO is full
+            end 
+            else begin
+                w_pop <= 1'b0;
+            end
+        end 
+        else begin
+            prev_wid <= prev_wid; // Maintain previous wid
+            w_push <= 1'b0; // Reset push flag if no new IDß
         end
     end
     else begin
-        rdat <= 0;
-        r_push <= 1'b0;
-        r_pop <= 1'b0;
+        w_push <= 1'b0; // Reset push flag if no handshake
     end
-
-    if(ar_trig_neg) begin
-        ar_addr <= ARADDR;
-        ar_push <= 1'b1;
-        if(ar_full) begin
-            ar_pop <= 1'b1;
-        end
-    end
-    else begin
-        ar_addr <= 0;
-        ar_push <= 1'b0;
-        ar_pop <= 1'b0;
-    end
-
-    if(aw_trig_neg) begin
-        aw_addr <= ARADDR;
-        aw_push <= 1'b1;
-        if(aw_full) begin
-            aw_pop <= 1'b1;
-        end
-    end
-    else begin
-        aw_addr <= 0;
-        aw_push <= 1'b0;
-        aw_pop <= 1'b0;
-    end
-
-    if(w_trig_neg) begin
-        w_data <= WDATA;
-        w_push <= 1'b1;
-        if(w_full) begin
-            w_pop <= 1'b1;
-        end
-    end
-    else begin
-        w_data <= 0;
-        w_push <= 1'b0;
-        w_pop <= 1'b0;
-    end
-
 end
 
-
-// Logic to detect the first time the handshake occurs ->
-
-/*
-Use trigger_signal wire -> need this.
-Place a conditional operator that checks whether the slave is ready or not 
-if the slave isnt ready , i.e. ready == 0 , tie the trigg to ative 1
-If the slave is ready , then tie to the EXOR of both ready and valid
-EXOR both the VALID and the READY signals -> This signal remains active 1 , until both of them go to 1
-the case where both valid and ready are 0 is taken care of by the conditional assignment
-now use a negedge detector for detecting when the handshake occurs for the first time -> then push the parameters into fifo
-*/
+// Logic to detect the first time the handshake occurs -> If we see a new ID during a valid transactiuon (when the handshake signal is high)
 
 endmodule
